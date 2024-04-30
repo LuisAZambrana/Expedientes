678a0e92ad8870e69cc7bc39eabed18a1533384a
elunicomatute <167909984+elunicomatute@users.noreply.github.com>
2024-04-30 15:08:01 -0300

    cc
    nuevi

--- view/registracion/adrg_borrador_h/delete.php
+++ view/registracion/adrg_borrador_h/delete.php
@@ -1,14 +1,14 @@
 <?php
      require_once($_SERVER['DOCUMENT_ROOT']."/proyecto/config/db.php");
      $obj = new db();
-     $row = $obj->fcgetSql("SELECT 'm' as abm, adma_sector.* from adma_sector where sectorid =".$_GET['id'],1,2);
+     $row = $obj->fcgetSql("SELECT 'm' as abm, adrg_borrador_h.* from adrg_borrador_h where borradorid =".$_GET['id'],1,2);
      $row["baja"]=1;
      session_start();
      $row["usuarioid"]= $_SESSION['usuarioid'];
      $row["fecharegistro"]= date('Ymd');
-     $resultado=$obj->ConfiguracionProcedimientoAlmacenado("adma_sector",1,$row);
+     $resultado=$obj->ConfiguracionProcedimientoAlmacenado("adrg_borrador_h",1,$row);
 
      if ($resultado > 0) {
-          header("Location:/proyecto/view/maestros/adma_sector/index.php?id=".$row['sectorid']);
+          header("Location:/proyecto/view/registracion/adrg_borrador_h/index.php?id=".$row['borradorid']);
       }else{header("Location:show.php?id=".$resultado);}
 ?>
